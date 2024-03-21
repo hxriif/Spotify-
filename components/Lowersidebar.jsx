da1c222da7510b5008f6e9a7a6372662a6ebf965
hxriif <zaidbnuarifc@gmail.com>
2024-03-21 00:22:45 -0700

    Fetched-playlists

---
+++ components/Lowersidebar.jsx
@@ -0,0 +1,11 @@
+import { Home, Search, Plus, Library, Music } from 'lucide-react';
+
+const LowerSidebar = () => {
+  
+
+  return (
+   
+  );
+};
+
+export default LowerSidebar;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,38 @@
+
+
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
-  const{data:session}=useSession()
-  
+  const { data: session } = useSession()
+  const [X, setX] = useState('')
+  const [playlists, setplaylists] = useState([])
+
+  useEffect(() => {
+    async function f() {
+      if (session && session.accessToken) {
+        setX(session.accessToken)
+        const response=await fetch("https://api.spotify.com/v1/me/playlists",{
+          headers:{
+            Authorization:`Bearer ${session.accessToken}`
+          }
+        })
+        const data= await response.json()
+        setplaylists(data.items)
+      }
+    }
+    f()
+  }, [session])
+
+
+
   return (
-    <>
-   <main className="flex w-full  h-screen overflow-hidden  bg-black">
-     <div>sidebar</div>
-     <div>menu</div>
-   </main>
-   <div className="sticky bottom-0 h-24 w-full bg-red-200"></div>
-   </>
+    <main className=" w-full  h-screen overflow-hidden  bg-black">
+      <div>accesss:{X}</div>
+      <div className="items-center">
+        {playlists.map((playlist)=><div key={playlist.id}>{playlist.name}</div>)}
+      </div>
+    </main>
   )
 }
